e14112a62fb05512ca6ec3ecb89f78b409e6c585
CardioE <dioniziolc@outlook.com>
2025-02-24 21:24:07 -0300

    Create Perfil.java

---
+++ Perfil.java
@@ -0,0 +1,15 @@
+public class Perfil {
+
+    public static void main(String[] args) {
+
+        // Informações do aluno
+        String nome = "Lucas Dionizio";
+        int idade = 26;
+        String hobby  =" Paintball " ;
+
+        // Exibe as informações
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Hobby: " + hobby);
+    }
+}
